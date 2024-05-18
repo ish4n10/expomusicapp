5c127d3e0a7e52e03e27c5af8873999f2f96027d
Hurvashi Dewangan <hurvashidewangan8118@gmail.com>
2024-05-19 01:04:40 +0530

    Live Room added and toggle issue solved

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,33 +1,189 @@
-import { StatusBar, StyleSheet, Text, TouchableOpacity, View,Dimensions } from "react-native"
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+  ScrollView,
+  Image,
+} from "react-native"
 import { NavigationContainer, useNavigation } from "@react-navigation/native"
-import RoomNavigatorBar from './RoomNavigator';
-import NavBar from './AppNavBar';
-import { useEffect } from 'react';
+import RoomNavigatorBar from "./RoomNavigator"
+import NavBar from "./AppNavBar"
+import { useEffect, useState } from "react"
 
-export default function HomeScreen(){
-    const Navigation = useNavigation();
-    
-    useEffect(()=>{
-        StatusBar.setHidden(false)
-        StatusBar.setTranslucent(false)
-        StatusBar.setBackgroundColor('#110B1F')
-        StatusBar.setBarStyle('light-content')
-    })
-    return(
-        <View style = {styles.container}>
-           <RoomNavigatorBar/>
-           
-           <NavBar/>
+export default function HomeScreen() {
+  const Navigation = useNavigation()
+  const [activeTab, setActiveTab] = useState("live")
 
-        </View>
-    );
+  useEffect(() => {
+    StatusBar.setHidden(false)
+    StatusBar.setTranslucent(false)
+    StatusBar.setBackgroundColor("#110B1F")
+    StatusBar.setBarStyle("light-content")
+  })
+  return (
+    <View style={styles.container}>
+      <RoomNavigatorBar setHomeTab={setActiveTab} />
+      {activeTab == "live" && (
+        <ScrollView style={{ flex: 1, width: "100%" }}>
+          <View style={styles.roomContainer}>
+            <TouchableOpacity activeOpacity={0.7} style={[styles.room]}>
+              <Image
+                source={require("./../../assets/image1.png")}
+                style={styles.img}
+              />
+              <View style={styles.room1}></View>
+              <View style={styles.container1}>
+                <View style={styles.location}>
+                  <Text style={styles.text}>Nigeria Falls</Text>
+                </View>
+                <View style={styles.container2}>
+                  <View style={styles.person}>
+                    <Image
+                      source={require("./../../assets/person.jpg")}
+                      style={styles.img1}
+                    />
+
+                    <Text style={styles.text1}>Room Owner</Text>
+                  </View>
+                  <View style={styles.person1}>
+                    <Image
+                      source={require("./../../assets/person.jpg")}
+                      style={styles.img2}
+                    />
+                    <Image
+                      source={require("./../../assets/person.jpg")}
+                      style={styles.img2}
+                    />
+                    <Image
+                      source={require("./../../assets/person.jpg")}
+                      style={styles.img2}
+                    />
+
+                    <View style={styles.count}>
+                      <Text style={styles.text2}>678</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      )}
+      {activeTab == "join" && <></>}
+      <NavBar />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor: "#110B1F",
-       flex:1,
-        justifyContent:'space-between',
-        alignItems:'center',
-    }
+  container: {
+    backgroundColor: "#110B1F",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  img: {
+    width: "100%",
+    height: 142,
+    borderRadius: 18,
+    position: "absolute",
+  },
+  img1: {
+    resizeMode: "cover",
+    width: 34,
+    height: 34,
+    borderRadius: 18,
+    borderColor: "#915DFF",
+    borderWidth: 1,
+  },
+  img2: {
+    width: 25,
+    height: 25,
+    borderRadius: 18,
+    borderColor: "#915DFF",
+    borderWidth: 1,
+  },
+  roomContainer: {
+    width: "100%",
+    height: 142,
+    marginTop: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  room: {
+    width: "90%",
+    height: 142,
+    backgroundColor: "#d6d6d6",
+    borderRadius: 18,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  room1: {
+    width: "100%",
+    height: 142,
+    backgroundColor: "#915DFF45",
+    position: "absolute",
+    borderRadius: 18,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container1: {
+    width: "90%",
+    height: "90%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  location: {
+    width: "100%",
+  },
+  container2: {
+    width: "100%",
+    height: 34,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  person: {
+    width: "auto",
+    height: 34,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+  },
+  person1: {
+    width: "auto",
+    height: 34,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+ 
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  text1: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  text2: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+
+  count: {
+    backgroundColor: "#915DFF60",
+    borderRadius: 18,
+    width: 40,
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 })
--- components/home/RoomNavigator.tsx
+++ components/home/RoomNavigator.tsx
@@ -1,29 +1,42 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { NavigationContainer, useNavigation } from "@react-navigation/native"
 import React, { useState } from "react"
 
-export default function RoomNavigatorBar() {
-  const Navigation = useNavigation();
-  const [color, setColor] = useState(false)
+export default function RoomNavigatorBar( {setHomeTab} : {setHomeTab:any}) {
+  const Navigation = useNavigation()
+  const [color, setColor] = useState(true)
   return (
     <View style={styles.topBarContainer}>
-      <View style = {styles.topBar}>
-        
-        <TouchableOpacity onPress={()=>setColor(!color)}
-        
-        style = {[styles.button,{backgroundColor:color?'#915DFF':'#1B1130'}]}
-            
-          >
-          <Text style = {[styles.text,{color:color ? '#fff' : '#915DFF'}]}>Live Room</Text>
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          onPress={() => {
+            setColor(true)
+            setHomeTab("live")
+          }}
+          style={[
+            styles.button,
+            { backgroundColor: color ? "#915DFF" : "#1B1130" },
+          ]}
+        >
+          <Text style={[styles.text, { color: color ? "#fff" : "#915DFF" }]}>
+            Live Room
+          </Text>
         </TouchableOpacity>
-        
-    
-        <TouchableOpacity onPress={()=>setColor(!color)}
-        style = {[styles.button,{backgroundColor:!color?'#915DFF':'#1B1130'}]} >
-          <Text style = {[styles.text,{color:!color ? '#fff' : '#915DFF'}]}>Create/Join </Text>
+
+        <TouchableOpacity
+          onPress={() =>{ setColor(false)
+            setHomeTab("join")
+          }}
+          style={[
+            styles.button,
+            { backgroundColor: !color ? "#915DFF" : "#1B1130" },
+          ]}
+        >
+          <Text style={[styles.text, { color: !color ? "#fff" : "#915DFF" }]}>
+            Create/Join{" "}
+          </Text>
         </TouchableOpacity>
-        </View>
-      
+      </View>
     </View>
   )
 }
@@ -33,27 +46,25 @@ const styles = StyleSheet.create({
     height: 48,
     justifyContent: "center",
     alignItems: "center",
-  
-    
   },
-  topBar:{
-    width:'80%',
-    backgroundColor:'#1B1130',
-    height:50,
-    borderRadius:12, 
-    flexDirection:'row', 
-},
-  text:{
-    color:'#fff',
-    textAlign:'center',
-    fontSize:18,
-    fontWeight:'bold',
+  topBar: {
+    width: "80%",
+    backgroundColor: "#1B1130",
+    height: 50,
+    borderRadius: 12,
+    flexDirection: "row",
   },
-  button:{
-    justifyContent:'center',
-    alignItems:'center',
-    width:'50%',
-    height:50,
-    borderRadius:12,
+  text: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%",
+    height: 50,
+    borderRadius: 12,
   },
 })
